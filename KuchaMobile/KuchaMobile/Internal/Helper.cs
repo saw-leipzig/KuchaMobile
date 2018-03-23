786eeed9c2dce966169170c22a8d8d7293399902
Eric Bach <eric2.bach@gmail.com>
2018-03-23 12:35:49 +0100

    Implemented basic Http API + Login functionality

---
+++ KuchaMobile/KuchaMobile/Internal/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KuchaMobile.Internal
+{
+    static class Helper
+    {
+        public static string GetMD5Hash(string TextToHash)
+        {
+            MD5 md5Hash = MD5.Create();
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(TextToHash));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+    }
+}
--- KuchaMobile/KuchaMobile/UI/SettingsUI.cs
+++ KuchaMobile/KuchaMobile/UI/SettingsUI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acr.UserDialogs;
+using KuchaMobile.Internal;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
@@ -7,5 +9,29 @@ namespace KuchaMobile.UI
 {
     public class SettingsUI : ContentPage
     {
+        public SettingsUI()
+        {
+            StackLayout contentStack = new StackLayout();
+
+            Button loginButton = new Button();
+            loginButton.Text = "Login";
+            loginButton.Clicked += LoginButton_Clicked;
+            contentStack.Children.Add(loginButton);
+
+            Content = contentStack;
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            LoginResult x = await UserDialogs.Instance.LoginAsync("Login", "Logindaten eintragen");
+            if(x.Ok)
+            {
+                string username = x.LoginText;
+                string password = x.Password;
+
+                bool loginSuccess = Connection.Login(username, password);
+                UserDialogs.Instance.Toast("Login: " + loginSuccess.ToString());
+            }
+        }
     }
 }
