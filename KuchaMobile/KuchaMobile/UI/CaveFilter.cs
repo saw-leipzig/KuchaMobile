e16ab5db049239c907158939f5ba595a51aa6ea3
Eric Bach <eric2.bach@gmail.com>
2018-04-06 15:20:44 +0200

    Small code update

---
+++ KuchaMobile/KuchaMobile/UI/CaveFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace KuchaMobile.UI
+{
+    public class CaveFilter : ContentPage
+    {
+        public enum CAVE_FILTER_TYPE
+        {
+            SITE,
+            DISTRICT,
+            REGION
+        }
+
+        public CaveFilter(CAVE_FILTER_TYPE type, CaveSearchUI parent)
+        {
+            switch(type)
+            {
+                case CAVE_FILTER_TYPE.DISTRICT: Title = "Districts"; break;
+                case CAVE_FILTER_TYPE.REGION: Title = "Regions"; break;
+                case CAVE_FILTER_TYPE.SITE: Title = "Sites"; break;
+            }
+
+        }
+    }
+}
