5c834ccd973f6006b10dcc489557b9b9b2d63915
Eric Bach <eric2.bach@gmail.com>
2018-06-11 20:08:32 +0200

    Small changes to Settings - Feature Complete!

--- KuchaMobile/KuchaMobile/UI/SettingsUI.cs
+++ KuchaMobile/KuchaMobile/UI/SettingsUI.cs
@@ -5,37 +5,73 @@ using PCLStorage;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace KuchaMobile.UI
 {
     public class SettingsUI : ContentPage
     {
+        Label downloadStatusLabel;
         public SettingsUI()
         {
+            Title = "Einstellungen";
             StackLayout contentStack = new StackLayout();
 
-            Button loginButton = new Button();
-            loginButton.Text = "Login";
-            loginButton.Clicked += LoginButton_Clicked;
-            contentStack.Children.Add(loginButton);
+            downloadStatusLabel = new Label();
+            downloadStatusLabel.Text = "Daten vom " + Kucha.GetDataTimeStamp().ToShortDateString();
+            contentStack.Children.Add(downloadStatusLabel);
+
+            Button updateLocalDatabaseButton = new Button();
+            updateLocalDatabaseButton.Text = "Datenbank updaten";
+            updateLocalDatabaseButton.Clicked += UpdateLocalDatabaseButton_Clicked;
+            contentStack.Children.Add(updateLocalDatabaseButton);
+
+            Button clearAllNotesButton = new Button();
+            clearAllNotesButton.Text = "Eigene Notizen löschen";
+            clearAllNotesButton.Clicked += ClearAllNotesButton_Clicked;
+            contentStack.Children.Add(clearAllNotesButton);
 
             Button deleteLocalFilesButton = new Button();
-            deleteLocalFilesButton.Text = "Delete Local Files";
-            deleteLocalFilesButton.Clicked += DeleteLocalFilesButton_Clicked; ;
+            deleteLocalFilesButton.Text = "Delete All Local Files";
+            deleteLocalFilesButton.Clicked += DeleteLocalFilesButton_Clicked;
             contentStack.Children.Add(deleteLocalFilesButton);
 
             Content = contentStack;
         }
 
+        private void ClearAllNotesButton_Clicked(object sender, EventArgs e)
+        {
+            Settings.SavedNotesSetting = new List<NotesSaver>();
+            UserDialogs.Instance.Toast("Eigene Notizen zurückgesetzt!");
+        }
+
+        private async void UpdateLocalDatabaseButton_Clicked(object sender, EventArgs e)
+        {
+            UserDialogs.Instance.ShowLoading("Daten werden heruntergeladen...");
+            await Task.Run(async () =>
+            {
+                bool success = await Kucha.RefreshCaveData();
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    UserDialogs.Instance.HideLoading();
+                    if (success)
+                    {
+                        UserDialogs.Instance.Toast("Daten erfolgreich heruntergeladen!");
+                        downloadStatusLabel.Text = "Daten vom " + Kucha.GetDataTimeStamp().ToShortDateString();
+                    }
+                    else
+                    {
+                        UserDialogs.Instance.Toast("Daten konnten nicht heruntergeladen werden. Bitte Netzwerkverbindung prüfen.");
+                    }
+                });
+            });
+        }
+
         private void DeleteLocalFilesButton_Clicked(object sender, EventArgs e)
         {
             Kucha.RemoveAllData();
-        }
-
-        private void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            App.Current.MainPage = new LoginPage();
+            UserDialogs.Instance.Toast("Alle lokalen Daten gelöscht!");
         }
     }
 }
