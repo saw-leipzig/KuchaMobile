25dde8b7f944af60f3abd05cc0804ef1e963d279
Eric Bach <eric2.bach@gmail.com>
2018-07-13 10:00:04 +0200

    Added ZoomExtension to ImageUI

--- KuchaMobile/KuchaMobile/UI/ImageUI.cs
+++ KuchaMobile/KuchaMobile/UI/ImageUI.cs
@@ -11,17 +11,26 @@ namespace KuchaMobile.UI
         private Editor notesEditor;
         private RelatedImage image;
 
+        double currentScale = 1;
+        double startScale = 1;
+        double xOffset = 0;
+        double yOffset = 0;
+
         public ImageUI(RelatedImage image)
         {
             this.image = image;
             Title = "Image " + image.shortName;
             StackLayout imageLayout = new StackLayout();
             Image displayImage = new Image();
-            displayImage.Source = ImageSource.FromUri(new Uri(Connection.GetPaintedRepresentationImageURL(image.imageID, Helper.ScreenHeight)));
+            //displayImage.Source = ImageSource.FromUri(new Uri(Connection.GetPaintedRepresentationImageURL(image.imageID, Helper.ScreenHeight)));
+            displayImage.Source = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Homo_floresiensis_cave.jpg/800px-Homo_floresiensis_cave.jpg";
             displayImage.Aspect = Aspect.AspectFill;
             imageLayout.Children.Add(displayImage);
             imageLayout.Padding = new Thickness(0, 10, 0, 20);
             imageLayout.Spacing = 10;
+            var pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += PinchGesture_PinchUpdated;
+            imageLayout.GestureRecognizers.Add(pinchGesture);
 
             Frame editorFrame = new Frame();
             editorFrame.HasShadow = true;
@@ -45,6 +54,44 @@ namespace KuchaMobile.UI
             ScrollView imageScrollView = new ScrollView();
             imageScrollView.Content = imageLayout;
             Content = imageScrollView;
+        }
+
+        private void PinchGesture_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+                Content.TranslationX = targetX.Clamp(-Content.Width * (currentScale - 1), 0);
+                Content.TranslationY = targetY.Clamp(-Content.Height * (currentScale - 1), 0);
+
+                Content.Scale = currentScale;
+            }
+            if (e.Status == GestureStatus.Completed)
+            {
+                xOffset = Content.TranslationX;
+                yOffset = Content.TranslationY;
+            }
         }
 
         protected override void OnDisappearing()
